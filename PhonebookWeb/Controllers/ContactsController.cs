0e034a2ad0677e52ee4efdbcce134d9757fab358
mbambosiyabonga@gmail.com <mbambosiyabonga@gmail.com>
2019-03-19 14:06:09 +0200

    added client crud

--- PhonebookWeb/Controllers/ContactsController.cs
+++ PhonebookWeb/Controllers/ContactsController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+//using System.Net.Http.Formatting;
 using System.Web.Mvc;
 using PhonebookWeb.Models;
 namespace PhonebookWeb.Controllers
@@ -16,6 +19,7 @@ namespace PhonebookWeb.Controllers
         // GET: Contacts
         public async Task<ActionResult> Index()
         {
+
             List<ContactModel> contacts = new List<ContactModel>();
             using (var client = new HttpClient())
             {
@@ -26,7 +30,7 @@ namespace PhonebookWeb.Controllers
                 //Define request data format  
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                //Sending request to find web api REST service resource Contacts using HttpClient  
                 HttpResponseMessage Res = await client.GetAsync("api/Contacts");
 
                 //Checking the response is successful or not which is sent using HttpClient  
@@ -35,20 +39,78 @@ namespace PhonebookWeb.Controllers
                     //Storing the response details recieved from web api   
                     var EmpResponse = Res.Content.ReadAsStringAsync().Result;
 
-                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    //Deserializing the response recieved from web api and storing into the Contact list  
+                    contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
+
+                }
+            }
+        
+            return View(contacts);
+        }
+
+        private async Task<List<ContactModel>>GetContacts()
+        {
+           List<ContactModel> contacts = new List<ContactModel>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource Contacts using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Contacts");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Contact list  
                     contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
 
                 }
                 //returning the employee list to view 
             }
 
-            return View(contacts);
+
+            return contacts;
         }
 
         // GET: Contacts/Details/5
-        public ActionResult Details(int id)
+        public async Task<ActionResult> Details(int id)
         {
-            return View();
+            ContactModel contact = null;
+            List<ContactModel> contacts = new List<ContactModel>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource Contacts using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Contacts");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Contact list  
+                    contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
+                    contact = contacts.FirstOrDefault(x => x.ID == id);
+
+                }
+            }
+            return View(contact);
         }
 
         // GET: Contacts/Create
@@ -59,12 +121,38 @@ namespace PhonebookWeb.Controllers
 
         // POST: Contacts/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public async Task<ActionResult> Create(ContactModel model)
         {
             try
             {
-                // TODO: Add insert logic here
 
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url
+                    client.BaseAddress = new Uri(Baseurl);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format  
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource Contacts using HttpClient  
+                    //StringContent content = new StringContent(collection);
+                    string json =  JsonConvert.SerializeObject(model); ;
+                    HttpContent c = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage Res = await client.PostAsync("api/Contacts/Post", c).ConfigureAwait(false);
+
+                    //Checking the response is successful or not which is sent using HttpClient  
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api   
+                        var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list  
+                       // contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
+
+                    }
+                    //returning the employee list to view 
+                }
                 return RedirectToAction("Index");
             }
             catch
@@ -74,18 +162,73 @@ namespace PhonebookWeb.Controllers
         }
 
         // GET: Contacts/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int id)
         {
-            return View();
+            ContactModel contact = null;
+            List<ContactModel> contacts = new List<ContactModel>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource Contacts using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Contacts");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Contact list  
+                    contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
+                    contact = contacts.FirstOrDefault(x => x.ID == id);
+
+                }
+            }
+            return View(contact);
         }
 
         // POST: Contacts/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public async Task<ActionResult> Edit( ContactModel model)
         {
             try
             {
-                // TODO: Add update logic here
+                ContactModel contact = null;
+
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url
+                    client.BaseAddress = new Uri(Baseurl);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format  
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource Contacts using HttpClient  
+                    //StringContent content = new StringContent(collection);
+                    string json = JsonConvert.SerializeObject(model); ;
+                    HttpContent c = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage Res = await client.PostAsync("api/Contacts/Post", c).ConfigureAwait(false);
+
+                    //Checking the response is successful or not which is sent using HttpClient  
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api   
+                        var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list  
+                        // contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
+
+                    }
+                    //returning the employee list to view 
+                }
 
                 return RedirectToAction("Index");
             }
@@ -96,18 +239,73 @@ namespace PhonebookWeb.Controllers
         }
 
         // GET: Contacts/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            ContactModel contact = null;
+            List<ContactModel> contacts = new List<ContactModel>();
+
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource Contacts using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/Contacts");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Contact list  
+                    contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
+                    contact = contacts.FirstOrDefault(x => x.ID == id);
+
+                }
+            }
+            return View(contact);
         }
 
         // POST: Contacts/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public async Task<ActionResult> Delete(ContactModel model)
         {
             try
             {
-                // TODO: Add delete logic here
+                ContactModel contact = null;
+
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url
+                    client.BaseAddress = new Uri(Baseurl);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format  
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource Contacts using HttpClient  
+                    //StringContent content = new StringContent(collection);
+                    string json = JsonConvert.SerializeObject(model); ;
+                    HttpContent c = new StringContent(json, Encoding.UTF8, "application/json");
+                    HttpResponseMessage Res = await client.DeleteAsync("api/Contacts/"+ model.ID).ConfigureAwait(false);
+
+                    //Checking the response is successful or not which is sent using HttpClient  
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api   
+                        var EmpResponse = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list  
+                        // contacts = JsonConvert.DeserializeObject<List<ContactModel>>(EmpResponse);
+
+                    }
+                    //returning the employee list to view 
+                }
 
                 return RedirectToAction("Index");
             }
